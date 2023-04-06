b00d2a3f9ec123c374aaddaccc042cf14202964d
CMLTTN <cemaldim@gmail.com>
2023-04-06 10:54:59 +0300

    first commit

---
+++ condition.js
@@ -0,0 +1,33 @@
+// var button = document.getElementById("submit");
+// var a = document.getElementById("input1");
+// var b = document.getElementById("input2");
+// var result = document.getElementById("result");
+// function add(a, b) {
+//     return a + b;
+// }
+// button.addEventListener("click", function () {
+//     console.log("clicked");
+//     result.innerText = "".concat(add(+a.value, +b.value));
+// });
+// console.log("hello world");
+
+// const gun = 10;
+
+// if(gun <= 10){
+//      console.log("günaydın");
+// }else{
+//      console.log("tünaydın");
+// }
+
+// const sayi = 20202033
+
+// if (sayi % 2 ===0){
+//      console.log(`${sayi}:çift sayidir`);
+// }else{
+//      console.log(`${sayi}:tek sayidir`);
+
+// }
+
+
+
+
