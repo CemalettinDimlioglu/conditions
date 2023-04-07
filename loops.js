1f9c7ed52a284963a02948e76fda12fa6614e0f7
CMLTTN <cemaldim@gmail.com>
2023-04-07 11:41:20 +0300

    ++a

--- loops.js
+++ loops.js
@@ -6,6 +6,17 @@
 
 //? 10 defa merhaba
 
-for(let i =1 ; i<=10 ; i++){
+for(let i =11 ; i<=10 ; i++){
      console.log(`${i} Merhaba`);
 }
+
+// console.log("devam");
+
+for (let i = 1; i <= 10; ++i) {
+  console.log(`${i} Merhaba`);
+}
+
+let a = 1
+
+console.log(++a);
+console.log(a);
