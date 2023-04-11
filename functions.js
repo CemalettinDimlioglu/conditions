6112932628961832327ace1a43f513ae2f188311
CMLTTN <cemaldim@gmail.com>
2023-04-11 12:12:53 +0300

    return

--- functions.js
+++ functions.js
@@ -1,8 +1,39 @@
 //? function declaration
-selam()
-function selam(){
-     console.log("hello");
-}
+// selam()
+// function selam(){
+//      console.log("hello");
+// }
 
-selam()
-selam()
+// selam()
+// selam()
+
+// function tanısma(firstName, lastName ){
+
+//      console.log(` Hİ! ${firstName} ${lastName} ${NaN}`);
+// }
+// tanısma("cemalettin", "d", "2023");
+// tanısma("selahaddin", "d")
+
+// const name = prompt("your name:");
+// const surname =prompt ("your surnane");
+
+// tanısma(name, surname)
+
+//? expession
+// const selam = function(){
+//      console.log("hello")
+// }
+
+// selam()
+// selam()
+
+// let num = function (n) {
+//   return n % 2  ? "tek" : "cift";
+// };
+
+// console.log(num(4));
+let num = function (n) {
+  return n % 2  ? "tek" : "cift";
+};
+
+console.log(num(4));
